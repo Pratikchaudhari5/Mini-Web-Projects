eb1d10fa682dbd137732f618738cef784ed5fe2e
Pratik Chaudhari <104305163+Pratikchaudhari5@users.noreply.github.com>
2023-08-27 20:50:06 +0530

    Add files via upload
    this is my 13assignment.

---
+++ Assignment-13/script.js
@@ -0,0 +1,11 @@
+const video = document.getElementById('background-video');
+
+video.controls = false; // hide controls
+// this button are used for the custom controls
+video.addEventListener('play', () => {
+    
+});
+
+video.addEventListener('pause', () => {
+    
+});
